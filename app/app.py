492edf49fed91124f024948701a369922a6c7cc4
Rodrigo Cabrera Castaldoni <castaldoniro@gmail.com>
2022-11-07 21:02:49 -0300

    chore: initial files
    Signed-off-by: Rodrigo Cabrera Castaldoni <castaldoniro@gmail.com>

---
+++ app/app.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from flask import Flask, request
+from src.utilities import load_model
+
+app = Flask(__name__)
+estimator = load_model()
+
+@app.route('/',  methods=['GET'])
+def home():
+    return "ESTOU ESPERANDO REQUISICOES"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    body = request.get_json()
+
+    X = pd.DataFrame([body])
+    y = estimator.predict(X)
+
+    return f'Feature, Predita {y}'
+
---
+++ app/src/utilities.py
@@ -0,0 +1,8 @@
+import pickle
+
+def load_model():
+
+    with open("artifacts/model.pickle", "rb") as f:
+        estimator = pickle.load(f)
+
+    return estimator
