a0a39ab6465b085bcc2f21328434c9c0ede1a7bd
Rodrigo Cabrera Castaldoni <castaldoniro@gmail.com>
2022-11-16 20:52:21 -0300

    tests: melhorando o input dos tests
    Signed-off-by: Rodrigo Cabrera Castaldoni <castaldoniro@gmail.com>

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,6 +1,18 @@
+import json
 import pytest
 from main import app
+
+from pathlib import Path
+
+current_folder_path = Path(__file__).parent
 
 @pytest.fixture()
 def client():
     return app.test_client()
+
+@pytest.fixture()
+def correct_input():
+    path = current_folder_path / "data" / "correct_input.json" 
+    with open(path, "rb") as f:
+        data = json.load(f)
+    return data
--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,6 +1,5 @@
 import json
 
-
 def test_home(client):
 
     response = client.get("/")
@@ -8,25 +7,10 @@ def test_home(client):
     assert 200 == response.status_code
     assert 'application/text' == response.content_type
 
-def test_predict(client):
-
-    body = [
-        {
-        "sepal length (cm)": 7.2,
-        "sepal width (cm)": 0.4,
-        "petal length (cm)": 0.2,
-        "petal width (cm)": 0.01
-        }, 
-        {
-        "sepal length (cm)": 7.2,
-        "sepal width (cm)": 20.4,
-        "petal length (cm)": 20.2,
-        "petal width (cm)": 0.01
-        }
-    ]
+def test_predict(client, correct_input):
 
     response = client.post("/predict", 
-                           data=json.dumps(body),
+                           data=json.dumps(correct_input),
                            content_type="application/json")
  
     assert {'predicts': '[0 2]'} == response.get_json()
