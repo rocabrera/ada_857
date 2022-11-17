d7036f6d7833e0fd6c464fc6fce1d0e322e56478
Rodrigo Cabrera Castaldoni <castaldoniro@gmail.com>
2022-11-16 21:16:48 -0300

    feat: tratamento de erros
    Signed-off-by: Rodrigo Cabrera Castaldoni <castaldoniro@gmail.com>

--- app/main.py
+++ app/main.py
@@ -2,9 +2,14 @@ import json
 import pandas as pd
 from flask import Flask, request, Response
 from src.utilities import load_model
+from src.exceptions import exception_handler, PredictionError
 
 app = Flask(__name__)
 estimator = load_model()
+
+@app.errorhandler(Exception)
+def handle_bad_request(e):
+    return exception_handler(e)
 
 @app.route('/',  methods=['GET'])
 def home():
@@ -16,15 +21,21 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    body = request.get_json()
-    X = pd.DataFrame(body)
-    y = estimator.predict(X)
+    try:
 
-    return Response(
-        response=json.dumps({"predicts":str(y)}),
-        status=200,
-        content_type='application/json'
-    )
+        body = request.get_json()
+        X = pd.DataFrame(body)
+        y = estimator.predict(X)
+
+    except Exception:
+        raise PredictionError("Check the body requisition")
+
+    else:
+        return Response(
+            response=json.dumps({"predicts": str(y)}),
+            status=200,
+            content_type='application/json'
+        )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
---
+++ app/src/exceptions.py
@@ -0,0 +1,21 @@
+from flask import Response
+
+class PredictionError(Exception):
+    pass
+
+def exception_handler(ex: Exception):
+    
+    if isinstance(ex, PredictionError):
+
+        return Response(
+            response=str(ex),
+            status=400,
+            content_type='application/text'
+        )
+
+    else:  
+        return Response(
+            response="Unknown Error",
+            status=500,
+            content_type='application/text'
+        )
