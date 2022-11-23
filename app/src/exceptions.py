a0f06849b5c36b8e6d01fd7505aac4ffe08b055f
Rodrigo Cabrera Castaldoni <castaldoniro@gmail.com>
2022-11-23 19:52:13 -0300

    chore: apply linter
    Signed-off-by: Rodrigo Cabrera Castaldoni <castaldoniro@gmail.com>

--- app/src/exceptions.py
+++ app/src/exceptions.py
@@ -1,21 +1,23 @@
 from flask import Response
 
+
 class PredictionError(Exception):
     pass
 
+
 def exception_handler(ex: Exception):
-    
+
     if isinstance(ex, PredictionError):
 
         return Response(
             response=str(ex),
             status=400,
-            content_type='application/text'
+            content_type="application/text"
         )
 
-    else:  
+    else:
         return Response(
             response="Unknown Error",
             status=500,
-            content_type='application/text'
+            content_type="application/text"
         )
